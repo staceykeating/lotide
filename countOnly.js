88ca01834f8d02e626ff925b8d39ac1345ec98cf
Stacey Keating <staceyamberkk@gmail.com>
2020-04-01 23:32:08 +0000

    object count and comparison created

---
+++ countOnly.js
@@ -0,0 +1,44 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🤠🤠🤠Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡😡😡Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {}
+
+  for (item of allItems) {
+    if (itemsToCount[item]) {
+    if (results[item]) {
+      // This is the first time we see item
+      results[item]++;
+    } else {
+      results[item] = 1;
+    }
+  }
+    // let x = 0;
+    // results[item] = x + 1
+    console.log(results);
+  }
+  return results;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
