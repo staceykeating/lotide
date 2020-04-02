618e6658946b3b62093d60a37f4afbc403a98059
Stacey Keating <staceyamberkk@gmail.com>
2020-04-02 04:46:02 +0000

    return key of object matching the keyvalue arg

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🤠🤠🤠Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡😡😡Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function (objectArray, keyValue) {
+    for (var key in objectArray) {
+        if(objectArray[key] == keyValue){
+            return key;
+        }
+    }
+ return undefined;
+}
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
